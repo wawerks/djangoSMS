13c72a4f5574909f8235151884fcebd29ca0d3f9
wawerks <rhowinlumales@gmail.com>
2024-11-26 20:33:23 +0800

    First commit with some errors

---
+++ myproject/scores/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+import os
+from twilio.rest import Client
+# Create your models here.
+
+class Score (models.Model):
+    result = models.PositiveIntegerField()
+
+    def __str__(self):
+        return str(self.result)
+
+    def save(self, *args, **kwargs):
+        if self.result < 70:
+            account_sid = "ACc7e5f9c7b69dc3219b0c973c8b17aa6f"
+            # Your Auth Token from twilio.com/console
+            auth_token  = "007efaaa8be27c0dd907720791683f0a"
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+                body=f"Test - {self.result} HAHAHHA",
+                from_="+19787881931",
+                to="+639630986870",
+            )
+
+            print(message.body)
+        return super().save(*args, **kwargs)
