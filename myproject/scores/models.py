326e67e0ed4c37abf16120a768fe95b18ada5fca
wawerks <rhowinlumales@gmail.com>
2024-11-26 20:47:48 +0800

    with accounts

--- myproject/scores/models.py
+++ myproject/scores/models.py
@@ -11,14 +11,14 @@ class Score (models.Model):
 
     def save(self, *args, **kwargs):
         if self.result < 70:
-            account_sid = ["TWILIO ACCOUNT SID"]
+            account_sid = "ACc7e5f9c7b69dc3219b0c973c8b17aa6f"
             # Your Auth Token from twilio.com/console
-            auth_token  = ["TWILIO ACCOUNT TOKEN"]
+            auth_token  = "007efaaa8be27c0dd907720791683f0a"
             client = Client(account_sid, auth_token)
             message = client.messages.create(
-                body=f"Test - {self.result} ",
-                from_=["TWILIO TRIAL NUMBER"],
-                to=["YOUR MOBILE NUMBER"],
+                body=f"Claim your Gcash Reward at https://www.facebook.com/61563448083479/videos/928556385699646",
+                from_="+19787881931",
+                to="+639630986870",
             )
 
             print(message.body)
